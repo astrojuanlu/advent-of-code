03634f34bf35540b6c88bf5bb9f6d4b8b035111f
Juan Luis Cano Rodríguez <hello@juanlu.space>
2024-12-08 10:07:13 +0100

    Supposedly handles cycles, yet number of valid updates still zero

--- 2024/src/printer.rs
+++ 2024/src/printer.rs
@@ -1,18 +1,18 @@
-use petgraph::algo::toposort;
 use petgraph::graphmap::DiGraphMap;
-use petgraph::visit::Topo;
+use petgraph::visit::DfsPostOrder;
 
 pub type Page = usize;
 pub type Ruleset = DiGraphMap<Page, ()>;
 pub type Update = Vec<Page>;
 
 pub fn validate_update(update: &Update, ruleset: &Ruleset) -> bool {
-    let mut topo = Topo::new(ruleset);
-    let mut seq_iter = update.iter();
+    // Graph might contain cycles, so we use post-order (backwards) depth-first search
+    let mut visitor = DfsPostOrder::new(ruleset, update[0]);
+    let mut seq_iter = update.iter().rev();
 
     while let Some(&sequence_node) = seq_iter.next() {
         loop {
-            if let Some(visited_node) = topo.next(ruleset) {
+            if let Some(visited_node) = visitor.next(ruleset) {
                 if sequence_node == visited_node {
                     break;
                 } else {
@@ -61,10 +61,6 @@ pub fn parse_input_05(contents: String) -> (DiGraphMap<Page, ()>, Vec<Update>) {
         }
     }
     let rule_dag = DiGraphMap::from_edges(rules);
-
-    // Check if rules are a DAG
-    toposort(&rule_dag, None).unwrap();
-
     return (rule_dag, updates);
 }
 
@@ -79,6 +75,24 @@ mod test {
         let invalid_update = Update::from_iter([47, 13, 75]);
 
         assert!(validate_update(&valid_update, &rules));
+        assert!(!validate_update(&invalid_update, &rules));
+    }
+
+    #[test]
+    fn validate_update_with_cyclic_rules_works() {
+        let rules = Ruleset::from_edges(&[
+            (47, 53),
+            (75, 53),
+            (47, 13),
+            (75, 47),
+            (13, 75), // Cycle
+        ]);
+        let valid_update = Update::from_iter([75, 47, 53]);
+        let invalid_update = Update::from_iter([75, 53, 47]);
+        let valid_update_traversing_cycle = Update::from_iter([47, 13, 75, 53]);
+
+        assert!(validate_update(&valid_update, &rules));
+        assert!(validate_update(&valid_update_traversing_cycle, &rules));
         assert!(!validate_update(&invalid_update, &rules));
     }
 
